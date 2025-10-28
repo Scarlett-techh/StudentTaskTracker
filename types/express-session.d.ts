215974b912871427e98d1a89236b7628537fb648
Scarlett-techh <scarlettwallace874@gmail.com>
2025-10-27 17:04:16 -0800

    express-session.d.ts

---
+++ types/express-session.d.ts
@@ -0,0 +1,15 @@
+// types/express-session.d.ts
+import 'express-session';
+
+declare module 'express-session' {
+  interface SessionData {
+    user: {
+      id: number;
+      email: string;
+      username: string;
+      firstName: string;
+      lastName: string;
+      userType: string;
+    };
+  }
+}
